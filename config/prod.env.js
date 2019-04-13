85628a459254f121ac8227dc726e6adc67c43646
newmac <bala223344@gmail.com>
2019-04-13 15:11:26 +0530

    remved config/

--- config/prod.env.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  RADIKS_SERVER:  "'http://localhost:1260'",
-  API_PATH: "'http://localhost:1260/api/'"
-  // RADIKS_SERVER:  "'/'",
-  // API_PATH: "'/api/'"
-
-
-}
