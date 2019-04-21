762b8af8df0a1e2ff645f34dfd90dbe50c6d5a8c
newmac <bala223344@gmail.com>
2019-04-21 08:02:59 +0530

    voting fix

--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -57,7 +57,7 @@
           >
             <p>{{answer.attrs.answer}}</p>
 
-            <span v-on:click="delete_answer(answer.attrs._id)">
+            <span class="trash-cont" v-on:click="delete_answer(answer.attrs._id)">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                 <path
                   d="M368 64l-33.6-44.8C325.3 7.1 311.1 0 296 0h-80c-15.1 0-29.3 7.1-38.4 19.2L144 64H40c-13.3 0-24 10.7-24 24v2c0 3.3 2.7 6 6 6h20.9l33.2 372.3C78.3 493 99 512 123.9 512h264.2c24.9 0 45.6-19 47.8-43.7L469.1 96H490c3.3 0 6-2.7 6-6v-2c0-13.3-10.7-24-24-24H368zM216 32h80c5 0 9.8 2.4 12.8 6.4L328 64H184l19.2-25.6c3-4 7.8-6.4 12.8-6.4zm188 433.4c-.7 8.3-7.6 14.6-15.9 14.6H123.9c-8.3 0-15.2-6.3-15.9-14.6L75 96h362l-33 369.4z"
@@ -258,7 +258,7 @@ export default {
       if (this.lock_voting) return;
 
       //convert null to 0
-      this.answers[i].vote_count = this.answers[i].vote_count || 0;
+      this.votes_for_answers[answer_id] = this.votes_for_answers[answer_id] || 0;
       this.lock_voting = true;
       var $icon;
       var $parent;
@@ -268,8 +268,6 @@ export default {
       $icon = $parent.find("#vote");
 
       $icon.hasClass("vote-none") && $icon.removeClass("vote-none");
-
-      console.log($icon.hasClass("vote-up"));
 
 
 
@@ -288,6 +286,8 @@ export default {
           //  this.answers[i].vote_count = parseInt(this.answers[i].vote_count) + 2;
           this.votes_for_answers[answer_id] += 2;
         } else {
+
+
           //this.answers[i].vote_count = parseInt(this.answers[i].vote_count) + 1;
           this.votes_for_answers[answer_id] += 1;
         }
@@ -298,7 +298,7 @@ export default {
       //  console.log('mdown');
       if (this.lock_voting) return;
       //convert null to 0
-      this.answers[i].vote_count = this.answers[i].vote_count || 0;
+      this.votes_for_answers[answer_id] = this.votes_for_answers[answer_id] || 0;
       this.lock_voting = true;
       var el = e.target;
       var com = this;
@@ -318,11 +318,9 @@ export default {
         //are we longpressing from an up vote..then we need a -2 bcz from 1 it should go to -1 not 0
 
         if ($icon.hasClass("vote-up") && $icon.removeClass("vote-up")) {
-          // com.answers[i].vote_count = com.answers[i].vote_count - 2;
           com.votes_for_answers[answer_id] -= 2;
         } else {
           com.votes_for_answers[answer_id] -= 1;
-          // com.answers[i].vote_count = com.answers[i].vote_count - 1;
         }
         com.castVote(answer_id, -1);
       } else {
